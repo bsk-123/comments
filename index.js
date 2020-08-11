78935c696736e604b4b0412b95070598575fb446
bsk-123 <43913935+bsk-123@users.noreply.github.com>
2020-08-11 19:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ document.querySelector(".autos").addEventListener("click",function(event){
     var st = ele.innerText;
     document.querySelector("input[type=text]").setAttribute("value",st)
 })
+var maxReplies=4
+var i
+for(i=1;i<maxReplies;i++)
+{
+    var st="r"+i
+    document.querySelector("."+st).style.display="none";
+}
+document.querySelector(".prevbtn").addEventListener("click",function(){
+    var ss = document.querySelector(".prevbtn span").innerText;
+    if(ss=="Previous"){
+        for(i=1;i<maxReplies;i++)
+        {
+            var st="r"+i
+            document.querySelector("."+st).style.display="block";
+        }
+    document.querySelector(".prevbtn span").innerText="Less";
+    }else{
+        document.querySelector(".prevbtn span").innerText="Previous";
+        for(i=1;i<maxReplies;i++)
+        {
+            var st="r"+i
+            document.querySelector("."+st).style.display="none";
+        }
+    }
+})
